6262bce09d16211b7f57739fd71772fed4642bad
David Riccitelli <david@wordlift.io>
2024-09-24 17:43:59 +0300

    add create_entity_patch_request

--- wordlift_sdk/utils/__init__.py
+++ wordlift_sdk/utils/__init__.py
@@ -1,3 +1,4 @@
 from .create_dataframe_of_url_id import create_dataframe_of_url_id
+from .create_entity_patch_request import create_entity_patch_request
 
-__all__ = ["create_dataframe_of_url_id"]
+__all__ = ["create_dataframe_of_url_id", "create_entity_patch_request"]
---
+++ wordlift_sdk/utils/create_entity_patch_request.py
@@ -0,0 +1,14 @@
+from rdflib import Graph, URIRef, Literal
+import wordlift_client
+
+
+def create_entity_patch_request(resource: URIRef, prop: URIRef, value: Literal):
+    g = Graph()
+    g.bind('schema', 'http://schema.org/')
+    g.add((resource, prop, value))
+
+    return wordlift_client.EntityPatchRequest(
+        op='add',
+        path='/' + str(prop),
+        value=g.serialize(format='json-ld', auto_compact=True)
+    )
