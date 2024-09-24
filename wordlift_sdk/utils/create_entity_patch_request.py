c82eab2a312a7a520888bf085dcbc36b98df536d
David Riccitelli <david@wordlift.io>
2024-09-24 21:40:40 +0300

    add return type

--- wordlift_sdk/utils/create_entity_patch_request.py
+++ wordlift_sdk/utils/create_entity_patch_request.py
@@ -2,7 +2,7 @@ from rdflib import Graph, URIRef, Literal
 import wordlift_client
 
 
-def create_entity_patch_request(resource: URIRef, prop: URIRef, value: Literal):
+def create_entity_patch_request(resource: URIRef, prop: URIRef, value: Literal) -> EntityPatchRequest:
     g = Graph()
     g.bind('schema', 'http://schema.org/')
     g.add((resource, prop, value))
