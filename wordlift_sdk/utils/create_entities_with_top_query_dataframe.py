e14ad79daa3887e5fe60274818bab4972e706bd1
David Riccitelli <david@wordlift.io>
2025-04-23 17:26:36 +0300

    fix method

--- wordlift_sdk/utils/create_entities_with_top_query_dataframe.py
+++ wordlift_sdk/utils/create_entities_with_top_query_dataframe.py
@@ -5,14 +5,15 @@ from pandas import DataFrame
 from tqdm.asyncio import tqdm
 
 from . import delayed
-from ..graphql.utils.query import entity_with_top_query
+from ..graphql.utils.query import entity_with_top_query_factory
 
 logger = logging.getLogger(__name__)
 
 
-async def create_entities_with_top_query_dataframe(url_list: list[str]) -> DataFrame:
+async def create_entities_with_top_query_dataframe(key: str, url_list: list[str]) -> DataFrame:
     # Get the entities data with the top query.
     logger.info("Loading entities with top query...")
+    entity_with_top_query = await entity_with_top_query_factory(key)
     entities_with_top_query = await tqdm.gather(
         *[delayed(entity_with_top_query, 4)(url) for url in url_list],
         total=len(url_list)
