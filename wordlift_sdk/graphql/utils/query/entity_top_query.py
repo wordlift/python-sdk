f197013b997ebe009a41188d187d7eb72f2649a6
David Riccitelli <david@wordlift.io>
2025-05-04 15:49:28 +0300

    fix dataframe creation; bump to 0.32.8

--- wordlift_sdk/graphql/utils/query/entity_top_query.py
+++ wordlift_sdk/graphql/utils/query/entity_top_query.py
@@ -46,7 +46,7 @@ class EntityTopQuery:
         )
 
     def to_dataframe(self) -> pd.DataFrame:
-        entities_with_top_query_df = pd.DataFrame(asdict(self))
+        entities_with_top_query_df = pd.DataFrame([asdict(self)])
         entities_with_top_query_df['calc_name'] = entities_with_top_query_df[
                                                       ['name', 'headline', 'title', 'url']].bfill(
             axis=1).iloc[:, 0]
