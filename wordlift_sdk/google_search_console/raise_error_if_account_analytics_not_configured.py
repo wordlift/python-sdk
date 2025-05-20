3baa41b4901f8ccf1148193d77baeeb9fa96b849
David Riccitelli <david@wordlift.io>
2025-05-20 15:09:01 +0300

    fix error messages

--- wordlift_sdk/google_search_console/raise_error_if_account_analytics_not_configured.py
+++ wordlift_sdk/google_search_console/raise_error_if_account_analytics_not_configured.py
@@ -4,16 +4,17 @@ from wordlift_client import AccountInfo
 
 async def raise_error_if_account_analytics_not_configured(account: AccountInfo) -> bool:
     if account.google_search_console_site_url is None:
-        raise ValueError("%s is not connected to Google Search Console, open https://my.wordlift.io to connect it.",
-                         account.dataset_uri)
+        raise ValueError(
+            "%s is not connected to Google Search Console, open https://my.wordlift.io to connect it." % account.dataset_uri)
 
     if account.country_code is None:
-        raise ValueError("%s country code not configured, open https://my.wordlift.io to configure it.",
-                         account.dataset_uri)
+        raise ValueError(
+            "%s country code not configured, open https://my.wordlift.io to configure it." % account.dataset_uri)
 
     # Get the country name
     country = countries.get(alpha_2=account.country_code.upper())
     if country is None:
-        raise ValueError("Country code %s is invalid, open https://my.wordlift.io to reconfigure it.")
+        raise ValueError(
+            "Country code %s is invalid, open https://my.wordlift.io to reconfigure it." % account.country_code.upper())
 
     return True
