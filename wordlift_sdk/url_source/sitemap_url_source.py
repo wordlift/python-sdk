f4b007cbf5bce06a50b3d98b2f4aa6093bace39c
David Riccitelli <david@wordlift.io>
2025-06-08 17:50:05 +0300

    ensure lastmod exists

--- wordlift_sdk/url_source/sitemap_url_source.py
+++ wordlift_sdk/url_source/sitemap_url_source.py
@@ -17,13 +17,20 @@ class SitemapUrlSource(UrlSource):
 
     async def urls(self) -> AsyncGenerator[Url, None]:
         sitemap_df = adv.sitemaps.sitemap_to_df(sitemap_url=self.sitemap_url)
-        sitemap_df['lastmod_as_datetime'] = pd.to_datetime(sitemap_df['lastmod'], errors='coerce')
+        # Ensure 'lastmod' column exists
+        if "lastmod" not in sitemap_df.columns:
+            sitemap_df["lastmod"] = None
+        sitemap_df["lastmod_as_datetime"] = pd.to_datetime(
+            sitemap_df["lastmod"], errors="coerce"
+        )
 
         for _, row in sitemap_df.iterrows():
-            url = row['loc']
-            last_mod_as_datetime = row['lastmod_as_datetime']
+            url = row["loc"]
+            last_mod_as_datetime = row["lastmod_as_datetime"]
             if self.pattern is None or self.pattern.search(url):
                 yield Url(
                     value=url,
-                    date_modified=None if pd.isna(last_mod_as_datetime) else last_mod_as_datetime.to_pydatetime()
+                    date_modified=None
+                    if pd.isna(last_mod_as_datetime)
+                    else last_mod_as_datetime.to_pydatetime(),
                 )
