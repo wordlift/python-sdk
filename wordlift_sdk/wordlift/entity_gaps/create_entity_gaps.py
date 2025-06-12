c36531bd935ee51df9ddebb571dfc49b92cfda6b
David Riccitelli <david@wordlift.io>
2025-06-12 18:03:58 +0300

    add back the factory

--- wordlift_sdk/wordlift/entity_gaps/create_entity_gaps.py
+++ wordlift_sdk/wordlift/entity_gaps/create_entity_gaps.py
@@ -11,29 +11,32 @@ from ...deprecated import create_entities_with_top_query_dataframe
 from ...utils import create_delayed
 
 
-async def create_entity_gaps(
-    key: str, configuration: Configuration, account: AccountInfo, url_list: list[str]
-) -> DataFrame:
-    # Get the entity data with the top query.
-    entities_with_top_query_df = await create_entities_with_top_query_dataframe(
-        key=key, url_list=url_list
-    )
+async def create_entity_gaps_factory(
+    key: str, configuration: Configuration, account: AccountInfo
+):
+    async def callback(url_list: list[str]) -> DataFrame:
+        # Get the entity data with the top query.
+        entities_with_top_query_df = await create_entities_with_top_query_dataframe(
+            key=key, url_list=url_list
+        )
 
-    country = countries.get(alpha_2=account.country_code.upper())
-    delayed = create_delayed(
-        await append_entity_gaps_response_to_row_factory(
-            entity_gaps_callback=await entity_gaps_callback_factory(
-                configuration=configuration, query_location_name=country.name
-            )
-        ),
-        2,
-    )
-    series = await tqdm.gather(
-        *[delayed(row) for index, row in entities_with_top_query_df.iterrows()],
-        total=len(entities_with_top_query_df),
-    )
+        country = countries.get(alpha_2=account.country_code.upper())
+        delayed = create_delayed(
+            await append_entity_gaps_response_to_row_factory(
+                entity_gaps_callback=await entity_gaps_callback_factory(
+                    configuration=configuration, query_location_name=country.name
+                )
+            ),
+            2,
+        )
+        series = await tqdm.gather(
+            *[delayed(row) for index, row in entities_with_top_query_df.iterrows()],
+            total=len(entities_with_top_query_df),
+        )
 
-    return DataFrame(series)
+        return DataFrame(series)
+
+    return callback
 
 
 async def append_entity_gaps_response_to_row_factory(
